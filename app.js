3b0fd4f9aa9ea84cdd5ad1f06adceecf6ccd37e4
beaztlyz <84481726+beaztlyz@users.noreply.github.com>
2023-11-14 13:12:11 -0700

    new

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const app = Vue.createApp({
     data()
     {
-    return {
-        about: "About/about.html",
-        labratory: "Labratory/labratory.html",
-        product: "Products/Products.html",
-        research: "Research/research.html",
-        solution: "Solutions/solutions.html",
-        login: "Login/login.html",
-        contact: "Contact/contact.html",
-        journal : "Journal/journal.html",
-        timeline : "Timeline/timeline.html"
-        };
+        return {
+            about: "About/about.html",
+            labratory: "Labratory/labratory.html",
+            product: "Products/Products.html",
+            research: "Research/research.html",
+            solution: "Solutions/solutions.html",
+            login: "Login/login.html",
+            contact: "Contact/contact.html",
+            journal : "Journal/journal.html",
+            timeline : "Timeline/timeline.html",
+            andes: "https://www.andestech.com/en/",
+            risc : "https://riscv.org"
+            };
     },
 });
 app.mount('#body');
