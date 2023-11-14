9c75472ea0a15e3c0c9280d1d6c96710d91dd9fb
beaztlyz <84481726+beaztlyz@users.noreply.github.com>
2023-11-14 12:01:26 -0700

    fix

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const app = Vue.createApp({
     data()
     {
-        return {
-            about: "About/about.html",
-            labratory: "Labratory/labratory.html",
-            product: "Products/Products.html",
-            research: "Research/research.html",
-            solution: "Solutions/solutions.html",
-            login: "Login/login.html",
-            contact: "Contact/contact.html",
-            journal : "Journal/journal.html",
-            timeline : "Timeline/timeline.html"
+    return {
+        about: "About/about.html",
+        labratory: "Labratory/labratory.html",
+        product: "Products/Products.html",
+        research: "Research/research.html",
+        solution: "Solutions/solutions.html",
+        login: "Login/login.html",
+        contact: "Contact/contact.html",
+        journal : "Journal/journal.html",
+        timeline : "Timeline/timeline.html"
         };
     },
 });
