9a3e4810b5dbea3d94bf5a31b8b1989a878175db
beaztlyz <84481726+beaztlyz@users.noreply.github.com>
2023-12-05 09:04:07 -0700

    new

--- app.js
+++ app.js
@@ -8,13 +8,13 @@ const app = Vue.createApp({
             research: "Research/research.html",
             solution: "Solutions/solutions.html",
             login: "Login/login.html",
-            register: "Register/register.html"
+            register: "Register/register.html",
             contact: "Contact/contact.html",
             journal : "Journal/journal.html",
             timeline : "Timeline/timeline.html",
             andes: "https://www.andestech.com/en/",
             risc : "https://riscv.org",
-            goals: "Goals/goals.html"
+            goals: "Goals/goals.html",
         };
     },
 });
